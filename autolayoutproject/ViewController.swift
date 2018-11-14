e07e8d463eb2983cee3679cc371fb888691435ef
Ashlirankin18 <ashlirankin18@gmail.com>
2018-11-14 14:23:11 -0500

    printed a sentence

---
+++ autolayoutproject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  autolayoutproject
+//
+//  Created by Ashli Rankin on 11/14/18.
+//  Copyright © 2018 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("omg we are doing git")
+        
+    }
+
+
+}
+
